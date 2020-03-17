9528a3e2b2be48092bd9e803f55fe59728a70fb7
EnjeruTantei <47004565+EnjeruTantei@users.noreply.github.com>
2020-03-17 13:38:28 -0400

    1.0.0
    Make Public

--- ExcelHandler.py
+++ ExcelHandler.py
@@ -1,7 +1,7 @@
 '''
 Created on Jan 15, 2019
 
-@author: Krystal
+@author: EnjeruTantei
 '''
 
 class ExcelHandler():
@@ -9,7 +9,7 @@ class ExcelHandler():
     classdocs
     '''
     import datetime
-    
+
     def __init__(self, name="Candle_Wic_Order_"+datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')+".xls", sheetName="CandleWicOrder"):
         '''
         Constructor
@@ -17,15 +17,15 @@ class ExcelHandler():
         import xlwt
         import datetime
         self.wb = xlwt.Workbook()
-        
+
         self.sheet1 = self.wb.add_sheet(sheetName)
         self.name = name
-        
+
     def savebook(self):
-        import os 
+        import os
         dir_path = os.path.dirname(os.path.realpath(__file__))
         self.wb.save(os.path.join(dir_path,self.name))
-    
+
     def write_row(self, col, row, val, sheet=None):
         if (sheet == None):
             sheet = self.sheet1
